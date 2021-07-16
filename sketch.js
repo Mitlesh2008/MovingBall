2e922a7ad9130d8fe28d9619be67fb66344a5327
Mitlesh2008 <87530002+Mitlesh2008@users.noreply.github.com>
2021-07-16 17:51:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var ball ;
+
+function setup() {
+  createCanvas(400,400);
+ball = createSprite (200,200,30,30)
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyIsDown (RIGHT_ARROW)) {
+    ball.x+=3
+  }
+
+  if (keyIsDown (LEFT_ARROW)) {
+    ball.x-=3
+  }
+
+  if (keyIsDown (UP_ARROW)) {
+    ball.y-=3
+  }
+  
+  if (keyIsDown (DOWN_ARROW)) {
+    ball.y+=3
+  }
+  
+
+ drawSprites();
+}
+
+
+
+
